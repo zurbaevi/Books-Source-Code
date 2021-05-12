9849e9a3d783d8f12264f6d4f51977f226136ce5
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-05-13 00:41:52 +0400

    Added chapter 11

---
+++ HeadFirstKotlin/src/main/kotlin/chapter11/Lambdas.kt
@@ -0,0 +1,44 @@
+package chapter11
+
+typealias DoubleConversion = (Double) -> Double
+
+fun convert(
+    x: Double,
+    converter: DoubleConversion
+): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+
+fun getConversionLambda(str: String): DoubleConversion {
+    if (str == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+fun combine(
+    lambda1: DoubleConversion,
+    lambda2: DoubleConversion
+): DoubleConversion {
+    return { x: Double -> lambda2(lambda1(x)) }
+}
+
+fun main(args: Array<String>) {
+    println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)}, US tons")
+}
