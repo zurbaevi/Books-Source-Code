5b54d6ce5089a473c0df8ca3ba486865d04bee05
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-05-17 17:28:09 +0400

    Added chapter 1

---
+++ AndroidProgrammingforprofessionalsKotlin/challenge1/src/main/java/com/bignerdranch/android/challenge1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.challenge1
+
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button;
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            val toast: Toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        }
+
+        falseButton.setOnClickListener {
+            val toast: Toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        }
+    }
+}
